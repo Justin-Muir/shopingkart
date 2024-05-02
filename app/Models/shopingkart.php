f2f20cf1fe9b09c878fd1ce383cccfaa55f76bfa
Justim-muir <justinsmuir@gmail.com>
2024-05-01 20:43:15 -0700

    first commit committing all files to the repo

---
+++ app/Models/shopingkart.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class shopingkart extends Model
+{
+    use HasFactory;
+}
