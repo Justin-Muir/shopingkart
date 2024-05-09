aa2b90adaf9504e4f4697dd3fbfd27cd793c0be1
Justim-muir <justinsmuir@gmail.com>
2024-05-08 23:11:08 -0700

    create list view and update navbar with signout button

--- resources/js/vue/CreateList.vue
+++ resources/js/vue/CreateList.vue
@@ -1,6 +1,23 @@
 <template >
     <div>
-        
+        <section class=" flex justify-center">
+            <div>
+                <form class="border-t-4 border-b-4 border-blue-100 rounded-lg">
+                    <div class="flex-col">
+                        <div class="grid mt-4">
+                            <input class="border-2 border-blue-300 bg-gray-100 rounded-lg w-80 p-2 shadow-lg focus:border-green-400" placeholder='LIST FOR' id="listfor" type="text">
+                        </div>
+                    </div>
+                    <div class="grid mt-5">
+                        <label class="text-green-500 font-bold font-weight-bolder text-xl" for="listdate">list date</label>
+                        <input class="border-2 border-blue-300 bg-gray-100 rounded-lg w-80 p-2 shadow-lg  focus:border-2 focus:border-green-400" id="listdate" type="date">
+                    </div>
+                    <div>
+                        <button class="bg-green-500 text-white p-3 m-3 rounded-lg font-bold hover:text-blue-700 hover:bg-white duration-500 shadow-lg">SAVE</button>
+                    </div>
+                </form>
+            </div>
+        </section>
     </div>
 </template>
 <script>
@@ -8,6 +25,3 @@ export default {
     name: "CreateList"
 }
 </script>
-<style >
-    
-</style>
--- resources/js/vue/NavBar.vue
+++ resources/js/vue/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav class="flex justify-between items-center">
+        <nav class="flex justify-between items-center border-b-4 border-green-100 mb-12">
             <div>
                 <ul class=" text-lg text-blue-500 font-bold ml-3 mt-4 flex item-center gap-4 cursor-pointer">
                     <router-link :to="{name:'Home'}" name="Home">Home</router-link>
@@ -8,7 +8,8 @@
                 </ul>
             </div>
             <div>
-                <button class="bg-green-500 text-white p-3 m-3 rounded-lg font-bold">Sign in</button>
+                <button class="bg-green-500 text-white p-3 m-3 rounded-lg font-bold hover:text-blue-700 hover:bg-white duration-500 shadow-lg">Sign in</button>
+                <button class="bg-green-500 text-white p-3 m-3 rounded-lg font-bold hover:text-blue-700 hover:bg-white duration-500 shadow-lg">Log out</button>
             </div>
         </nav>
     </div>
